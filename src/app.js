d96388c80b826240a6ffd1b65923324bf55ac38c
Mohammad Yaseen <yaseenyaseenali69@gmail.com>
2024-06-14 22:47:19 +0500

    Adding cors middleware

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
 import express from "express"
 import cookieParser from "cookie-parser"
+import cors from "cors"
 
 const app = express()
 
@@ -8,6 +9,10 @@ app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 app.use(express.static('public'))
 app.use(cookieParser())
+app.use(cors({
+    origin: "*",
+    credentials: true
+}))
 
 
 
